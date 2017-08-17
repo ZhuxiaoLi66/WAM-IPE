cd18b3c45ae85ffcc5783984f744962152c3ad44
Joe Schoonover <joseph.schoonover@noaa.gov>
2017-08-17 17:25:54 +0000

    Added files necessary for NEMSAppBuilder

--- DATAIPE/dataipe.mk
+++ DATAIPE/dataipe.mk
@@ -1,8 +1,8 @@
 # ESMF self-describing build dependency makefile fragment
 
 ESMF_DEP_FRONT     = DATAIPE
-ESMF_DEP_INCPATH   = /scratch3/NCEPDEV/swpc/noscrub/Naomi.Maruyama/esmf/apps20170215/20170817rt96661/naomi/DATAIPE
-ESMF_DEP_CMPL_OBJS = /scratch3/NCEPDEV/swpc/noscrub/Naomi.Maruyama/esmf/apps20170215/20170817rt96661/naomi/DATAIPE/dataipe.o
-ESMF_DEP_LINK_OBJS = /scratch3/NCEPDEV/swpc/noscrub/Naomi.Maruyama/esmf/apps20170215/20170817rt96661/naomi/DATAIPE/dataipe.o
+ESMF_DEP_INCPATH   = /home/Joseph.Schoonover/Software/testing/WAM-IPE/DATAIPE
+ESMF_DEP_CMPL_OBJS = /home/Joseph.Schoonover/Software/testing/WAM-IPE/DATAIPE/dataipe.o
+ESMF_DEP_LINK_OBJS = /home/Joseph.Schoonover/Software/testing/WAM-IPE/DATAIPE/dataipe.o
 ESMF_DEP_SHRD_PATH = 
 ESMF_DEP_SHRD_LIBS = 
---
+++ DATAWAM-INSTALL/datawam.mk
@@ -0,0 +1,8 @@
+# ESMF self-describing build dependency makefile fragment
+
+ESMF_DEP_FRONT     = DATAWAM
+ESMF_DEP_INCPATH   = ///home/Joseph.Schoonover/Software/testing/WAM-IPE/DATAWAM-INSTALL
+ESMF_DEP_CMPL_OBJS = ///home/Joseph.Schoonover/Software/testing/WAM-IPE/DATAWAM-INSTALL/datawam.o
+ESMF_DEP_LINK_OBJS = ///home/Joseph.Schoonover/Software/testing/WAM-IPE/DATAWAM-INSTALL/datawam.o
+ESMF_DEP_SHRD_PATH = 
+ESMF_DEP_SHRD_LIBS = 
