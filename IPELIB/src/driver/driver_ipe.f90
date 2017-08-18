c34031604c8aec7a0fbb2427e992fc48d58c4ad3
Joe Schoonover <joseph.schoonover@noaa.gov>
2017-08-18 20:11:02 +0000

    Added SMS statement in driver_ipe.f90 for setting MPI_COMM_IPE to MPI_COMM_WORLD for IPE standalone runs

--- IPELIB/src/driver/driver_ipe.f90
+++ IPELIB/src/driver/driver_ipe.f90
@@ -57,6 +57,7 @@ PROGRAM  test_plasma
 !SMS$INSERT parallelBuild=.true.
 ! set up input parameters
      ret = gptlstart ('read_input')
+!SMS$INSERT         MPI_COMM_IPE = MPI_COMM_WORLD
      CALL read_input_parameters ( )
      ret = gptlstop  ('read_input')
 
@@ -319,7 +320,7 @@ PROGRAM  test_plasma
 !ghgm - a dummy timestamp (13 characters) needs to be here
 ! because we use timestamps in the fully coupleid WAM-IPE
 ! Obviously needs a better solution.....
-        CALL plasma ( utime, 'dummytimestam' )
+        CALL plasma ( utime_driver, 'dummytimestam' )
         ret = gptlstop  ('plasma')
 
 !sms$compare_var(plasma_3d,"driver_ipe.f90 - plasma_3d-8")
@@ -335,9 +336,9 @@ PROGRAM  test_plasma
 #endif
 !SMS$IGNORE END
 
-       IF( MOD(REAL(utime,real_prec),dumpFrequency)==0)THEN
-          WRITE( iterChar, '(I8.8)' ) iterate
-          CALL io_plasma_bin ( 1, utime, 'iter_'//iterChar )
+       IF( MOD(REAL(utime_driver,real_prec),dumpFrequency)==0)THEN
+          WRITE( iterChar, '(I8.8)' )utime_driver
+          CALL io_plasma_bin ( 1, utime_driver, 'iter_'//iterChar )
        ENDIF
        ret = gptlstart ('output')
        CALL output ( utime_driver )
