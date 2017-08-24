d7ae236149f6445d553f7e6f7f86e8f0fdff4c90
oehmke <Robert.Oehmke@noaa.gov>
2017-08-24 19:52:39 +0000

    Change 2013 1hr compset to be restartable and to work with new IPE files. Also add 2013 1hr restart compset.

--- NEMS/src/conf/modules.nems.sh
+++ NEMS/src/conf/modules.nems.sh
@@ -1,3 +1,3 @@
-source /home/Joseph.Schoonover/Software/WAM-IPE/NEMS/src/conf/module-setup.sh.inc
-module use /home/Joseph.Schoonover/Software/WAM-IPE/NEMS/src/conf
+source /scratch3/NCEPDEV/swpc/noscrub/Robert.Oehmke/bob_ms06/NEMS/src/conf/module-setup.sh.inc
+module use /scratch3/NCEPDEV/swpc/noscrub/Robert.Oehmke/bob_ms06/NEMS/src/conf
 module load modules.nems
