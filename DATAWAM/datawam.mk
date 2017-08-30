2ce1a983094c23f29ec70810c1ee6f0e2ca65f95
Adam Kubaryk <Adam.Kubaryk@noaa.gov>
2017-08-30 10:09:18 -0400

    Changes to the build system to support WCOSS, and machine-agnosticize the RT_WAM folder.

--- DATAWAM/datawam.mk
+++
@@ -1,8 +0,0 @@
-# ESMF self-describing build dependency makefile fragment
-
-ESMF_DEP_FRONT     = DATAWAM
-ESMF_DEP_INCPATH   = /scratch3/NCEPDEV/swpc/noscrub/Robert.Oehmke/bob_ms06_commit/DATAWAM
-ESMF_DEP_CMPL_OBJS = /scratch3/NCEPDEV/swpc/noscrub/Robert.Oehmke/bob_ms06_commit/DATAWAM/datawam.o
-ESMF_DEP_LINK_OBJS = /scratch3/NCEPDEV/swpc/noscrub/Robert.Oehmke/bob_ms06_commit/DATAWAM/datawam.o
-ESMF_DEP_SHRD_PATH = 
-ESMF_DEP_SHRD_LIBS = 
