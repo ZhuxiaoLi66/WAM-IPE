cd5f1fa52064fea5fd17f8a6e24fe5db8acc4b9e
Adam Kubaryk <Adam.Kubaryk@noaa.gov>
2017-09-08 14:04:28 -0400

    new compset system

---
+++ scripts/compsets/submit.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+CONFIG=$1
+
+## source config
+. $CONFIG
+
+## create job file
+tmp=temp_job.sh
+rm -rf $tmp
+touch $tmp
+chmod +x $tmp
+
+## set up PBS/LSF/whatever
+echo "#!/bin/bash" > $tmp
+# the below is a little hacky but it sure works
+d=0
+while
+        d=$((d+1))
+        eval SUBFLAG=\$SUBFLAG$d
+        [[ -n "$SUBFLAG" ]]
+do
+        echo `eval echo $SUBFLAG` >> $tmp
+done
+## and now back to the regularly scheduled program
+cat >> $tmp << EOF
+set -ax
+
+cd $SCRIPTSDIR
+
+##-------------------------------------------------------
+## source config file
+##-------------------------------------------------------
+
+. $CONFIG
+
+##-------------------------------------------------------
+## execute forecast
+##-------------------------------------------------------
+
+mkdir -p $RUNDIR
+cd $RUNDIR
+
+export VERBOSE=YES
+
+. $EXGLOBALFCSTSH
+if [ $? != 0 ]; then echo "forecast failed, exit"; exit; fi
+echo "fcst done"
+
+exit $status
+EOF
+
+$SCHEDULER_SUB < $tmp
+rm -rf $temp
