1bf942a5fb6121b5923dcfae38d0bd30e0acd671
Adam Kubaryk <Adam.Kubaryk@noaa.gov>
2017-09-08 15:19:42 -0400

    tidying up some errors in the theia compset

--- DATAIPE-INSTALL/dataipe.mk
+++ DATAIPE-INSTALL/dataipe.mk
@@ -1,8 +1,8 @@
 # ESMF self-describing build dependency makefile fragment
 
 ESMF_DEP_FRONT     = DATAIPE
-ESMF_DEP_INCPATH   = ///scratch3/NCEPDEV/swpc/noscrub/Robert.Oehmke/bob_ms06_commit/DATAIPE-INSTALL
-ESMF_DEP_CMPL_OBJS = ///scratch3/NCEPDEV/swpc/noscrub/Robert.Oehmke/bob_ms06_commit/DATAIPE-INSTALL/dataipe.o
-ESMF_DEP_LINK_OBJS = ///scratch3/NCEPDEV/swpc/noscrub/Robert.Oehmke/bob_ms06_commit/DATAIPE-INSTALL/dataipe.o
+ESMF_DEP_INCPATH   = ///scratch3/NCEPDEV/swpc/save/Adam.Kubaryk/nems/mdate_compatible_timestamp/WAM-IPE/DATAIPE-INSTALL
+ESMF_DEP_CMPL_OBJS = ///scratch3/NCEPDEV/swpc/save/Adam.Kubaryk/nems/mdate_compatible_timestamp/WAM-IPE/DATAIPE-INSTALL/dataipe.o
+ESMF_DEP_LINK_OBJS = ///scratch3/NCEPDEV/swpc/save/Adam.Kubaryk/nems/mdate_compatible_timestamp/WAM-IPE/DATAIPE-INSTALL/dataipe.o
 ESMF_DEP_SHRD_PATH = 
 ESMF_DEP_SHRD_LIBS = 
--- NEMS/src/conf/modules.nems.sh
+++ NEMS/src/conf/modules.nems.sh
@@ -1,3 +1,3 @@
-source /scratch3/NCEPDEV/swpc/noscrub/Robert.Oehmke/bob_ms06_commit/NEMS/src/conf/module-setup.sh.inc
-module use /scratch3/NCEPDEV/swpc/noscrub/Robert.Oehmke/bob_ms06_commit/NEMS/src/conf
+source /scratch3/NCEPDEV/swpc/save/Adam.Kubaryk/nems/mdate_compatible_timestamp/WAM-IPE/NEMS/src/conf/module-setup.sh.inc
+module use /scratch3/NCEPDEV/swpc/save/Adam.Kubaryk/nems/mdate_compatible_timestamp/WAM-IPE/NEMS/src/conf
 module load modules.nems
