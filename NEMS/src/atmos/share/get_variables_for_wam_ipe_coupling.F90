3af2d59cc29b81c47b05732e6f1c1aca940795b7
Joe Schoonover <joseph.schoonover@noaa.gov>
2018-01-25 18:38:23 +0000

    Fixed type mismatch issue, but there are two variables declared within gsm that also need to be exposed to module_ATM_GRID_COMP.F90

--- NEMS/src/atmos/share/get_variables_for_wam_ipe_coupling.F90
+++ NEMS/src/atmos/share/get_variables_for_wam_ipe_coupling.F90
@@ -1,9 +1,10 @@
       module get_variables_for_WAM_IPE_coupling
 
+      USE ESMF
       IMPLICIT NONE
 
-      REAL, DIMENSION(:, :, :), ALLOCATABLE :: wwg, zzg, uug,vvg, ttg, rqg, n2g, ppg
-      REAL, DIMENSION(:, :),    ALLOCATABLE :: ps
+      REAL(ESMF_KIND_R8), DIMENSION(:, :, :), ALLOCATABLE :: wwg, zzg, uug,vvg, ttg, rqg, n2g, ppg
+      REAL(ESMF_KIND_R8), DIMENSION(:, :),    ALLOCATABLE :: ps
 
       END module get_variables_for_WAM_IPE_coupling
 
